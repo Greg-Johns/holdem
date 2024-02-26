d166fc3b4ebd9891bc16548217e028d679300cec
Greg Johns <greg.johns@cbre.com>
2024-02-26 14:16:42 -0700

    connecting w just ethers

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,69 @@
+import { useState, useEffect } from "react";
 import type { NextPage } from 'next'
 import Link from "next/link";
-import Head from 'next/head'
+import Head from 'next/head';
+import { ethers } from "ethers";
+import * as sapphire from '@oasisprotocol/sapphire-paratime';
+
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.css';
+
+
+const HARDHAT_NETWORK_ID = '23295'; // Sapphire Testnet
 
 const Home: NextPage = () => {
+
+  const [hasMetamask, sethasMetamask] = useState(true);
+
+  const [client, setclient] = useState({
+    isConnected: false,
+  });
+
+  const checkConnection = async () => {
+    const { ethereum } = window;
+    if (ethereum) {
+      sethasMetamask(true);
+      const accounts = await ethereum.request({ method: "eth_accounts" });
+      if (accounts.length > 0) {
+        setclient({
+          isConnected: true,
+          address: accounts[0],
+        });
+      } else {
+        setclient({
+          isConnected: false,
+        });
+      }
+    } else {
+      sethasMetamask(false);
+    }
+  };
+
+  const connectWeb3 = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        console.log("Metamask not detected");
+        return;
+      }
+
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+
+      setclient({
+        isConnected: true,
+        address: accounts[0],
+      });
+    } catch (error) {
+      console.log("Error connecting to metamask", error);
+    }
+  };
+
+  useEffect(() => {
+    checkConnection();
+  }, []);
 
   function create_game() {
     console.log('create game')
@@ -22,37 +81,71 @@ const Home: NextPage = () => {
 
         <h1>Texas Holdem</h1>
 
-        <form className={styles.form}>
-          <label>
-            Player Count
-          </label>
-          <input
-            id="player_count"
-            type="text"
-            placeholder="Player Count"
-          />
-          <label>
-            Buy In
-          </label>
-          <input
-            id="buy_in"
-            type="text"
-            placeholder="Buy In Value"
-          />
-          <button
-            onClick={() => create_game()}
-            type="button"
-          >
-            Create Table
-          </button>
-        </form>
+        <button className="btn connect-btn" onClick={connectWeb3}>
+          {client.isConnected ? (
+            <>
+              {client.address.slice(0, 4)}...
+              {client.address.slice(38, 42)}
+            </>
+          ) : (
+            <>Connect Wallet</>
+          )}
+        </button>
 
-        <footer className={styles.footer}>
-          <Link href="/fish_faucet">Fish faucet</Link>
-          <Link href="/table">table</Link>
-          <Link href="/about">About</Link>
-        </footer>
+        <p>
+          {!hasMetamask ? (
+            <Metamask />
+          ) : client.isConnected ? (
+            <>
+              <h2>You're connected ✅</h2>
+              <button
+                onClick={console.log("signMessage")}
+                type="button"
+              >
+                Sign Message
+              </button>
+            </>
+          ) : (
+            <>
+              <br />
+              <button onClick={connectWeb3}>
+                Connect Wallet
+              </button>
+            </>
+          )}
+        </p>
 
+        {/* <form className={styles.form}> */}
+        {/*   <label> */}
+        {/*     Player Count */}
+        {/*   </label> */}
+        {/*   <input */}
+        {/*     id="player_count" */}
+        {/*     type="text" */}
+        {/*     placeholder="Player Count" */}
+        {/*   /> */}
+        {/*   <label> */}
+        {/*     Buy In */}
+        {/*   </label> */}
+        {/*   <input */}
+        {/*     id="buy_in" */}
+        {/*     type="text" */}
+        {/*     placeholder="Buy In Value" */}
+        {/*   /> */}
+        {/*   <button */}
+        {/*     onClick={() => create_game()} */}
+        {/*     type="button" */}
+        {/*   > */}
+        {/*     Create Table */}
+        {/*   </button> */}
+        {/* </form> */}
+        {/**/}
+        {/* <footer className={styles.footer}> */}
+        {/*   <Link href="/fish_faucet">Fish faucet</Link> */}
+        {/*   <Link href="/table">table</Link> */}
+        {/*   <Link href="/about">About</Link> */}
+        {/* </footer> */}
+        {/**/}
       </main>
 
     </div>
